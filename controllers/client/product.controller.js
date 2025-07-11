c388ef618effba394972baecc91065649317886f
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-07-11 22:11:57 +0700

    feat: Trang danh sach san pham ben client

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -1,5 +1,18 @@
-module.exports.index = (req, res) => {
+const Product = require("../../models/product.model");
+
+module.exports.index = async (req, res) => {
+    const products = await Product.find({
+        status: "active",
+        deleted: false
+    });
+
+    products.forEach(item => {
+        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
+    })
+    console.log(products);
+
     res.render("client/pages/products/index", {
-        pageTitle: "Danh sách sản phẩm"
+        pageTitle: "Danh sách sản phẩm",
+        products: products
     });
 }
--- models/product.model.js
+++ models/product.model.js
@@ -1,8 +1,15 @@
 const mongoose = require("mongoose");
 
 const productSchema = new mongoose.Schema({
-  name: String,
-  size: String
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  stock: Number,
+  thumbnail: String,
+  status: String,
+  position: Number,
+  deleted: Boolean
 });
 
 const Product = mongoose.model("Product", productSchema, "products");
