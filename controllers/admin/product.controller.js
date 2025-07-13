75ec5182f99873f8a90e6d7c128b4b670baf1e76
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-07-13 21:50:10 +0700

    feat: load products ra giao dien admin/products

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -2,7 +2,9 @@
 const Product = require("../../models/product.model");
 
 module.exports.index = async (req, res) => {  
-    const products = await Product.find();
+    const products = await Product.find({
+        deleted: false,
+    });
    
     products.forEach(item => {
         item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
