32a3376a783f54e00c570a1ae642295e4a7db56b
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-07-14 15:45:19 +0700

    feat: add fillter product

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,17 +1,45 @@
 // [GET] /admin/products
 const Product = require("../../models/product.model");
 
-module.exports.index = async (req, res) => {  
-    const products = await Product.find({
+module.exports.index = async (req, res) => {
+    let find = {
         deleted: false,
-    });
-   
+    }
+    let fillterStatus = [
+        {
+            name: "Tất cả",
+            status: "",
+            class: ""
+        },
+        {
+            name: "Hoạt động",
+            status: "active",
+            class: ""
+        },
+        {
+            name: "Dừng hoạt động",
+            status: "inactive",
+            class: ""
+        }
+    ];
+    if (req.query.status) {       
+        const index = fillterStatus.findIndex(item => item.status == req.query.status);
+        fillterStatus[index].class = "active";
+        find.status = req.query.status;
+    }else{
+        const index = fillterStatus.findIndex(item => item.status == "");
+        fillterStatus[index].class = "active";
+    }
+    
+    const products = await Product.find(find);
+
     products.forEach(item => {
-        item.priceNew = (item.price*(100 - item.discountPercentage)/100).toFixed(0);
+        item.priceNew = (item.price * (100 - item.discountPercentage) / 100).toFixed(0);
     });
 
     res.render("admin/pages/products/index", {
         pageTitle: "Danh sách sản phẩm",
         products: products,
+        fillterStatus: fillterStatus
     });
 }
