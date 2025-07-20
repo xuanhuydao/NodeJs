3aa8fbd12396bd5034c2c46228dd989a7db741ec
XuanHuy <daoxuanhuy2004zz@gmail.com>
2025-07-20 21:32:20 +0700

    toi uu tinh nang tim kiem

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -1,25 +1,22 @@
 const Product = require("../../models/product.model");
 const filterStatusHelper = require("../../helpers/filterStatus");
-
+const searchHelper = require("../../helpers/search");
 // [GET] /admin/products
 module.exports.index = async (req, res) => {
     let find = {
         deleted: false,
     }
+
     const filterStatus = filterStatusHelper(req.query);
 
     if(req.query.status) {
         find.status = req.query.status;
     }
-    let keyword = "";
-    if(req.query.keyword) {
-        keyword = req.query.keyword;
-        const regex = new RegExp(keyword, "i");
-        find.title = regex;
-    }else{
-         keyword = "";
-    }
     
+    const objectSearch = searchHelper(req.query);
+    if(objectSearch.keyword !== "") {
+        find.title = objectSearch.regex;
+    }
     const products = await Product.find(find);
 
     products.forEach(item => {
@@ -32,6 +29,6 @@ module.exports.index = async (req, res) => {
         pageTitle: "Danh sách sản phẩm",
         products: products,
         filterStatus: filterStatus,
-        keyword: keyword
+        keyword: objectSearch.keyword
     });
 }
